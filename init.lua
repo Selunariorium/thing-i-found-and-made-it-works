7155f45ec1479046e6433cdb5ccdbd55f556f3e7
Selunar <156382268+Selunariorium@users.noreply.github.com>
2025-07-03 15:15:53 +0700

    update init.lua

---
+++ init.lua
@@ -0,0 +1,7 @@
+local Bzzt = {
+	Bolt = loadstring(game:HttpGet('https://raw.githubusercontent.com/Selunariorium/thing-i-found-and-made-it-works/refs/heads/main/LightningBolt.lua', 'Bolt'))(),
+	Sparks = loadstring(game:HttpGet('https://raw.githubusercontent.com/Selunariorium/thing-i-found-and-made-it-works/refs/heads/main/LightningSparks.lua', 'Sparks'))(),
+	Explosion = loadstring(game:HttpGet('https://raw.githubusercontent.com/Selunariorium/thing-i-found-and-made-it-works/refs/heads/main/LightningExplosion.lua', 'Sparks'))()
+}
+
+return Bzzt
